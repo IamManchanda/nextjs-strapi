ab6bd115847ad4ac34a3480b900421bed9b812ff
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-06 00:09:28 +0530

    Update webpack config for alias

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@ const path = require("path");
 
 module.exports = {
   webpack: (config) => {
-    config.resolve.alias["components"] = path.join(
-      __dirname,
-      "src",
-      "components",
-    );
-    config.resolve.alias["public"] = path.join(__dirname, "src", "public");
+    config.resolve.alias["@"] = path.join(__dirname, "src");
     return config;
   },
 };
