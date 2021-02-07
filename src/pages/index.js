d151fe32fb061bda01217ee993a63ebfbdc2d14e
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-08 00:24:18 +0530

    Starting working progress on data fetching setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
-function PageIndex() {
+function PageIndex({ movies }) {
+  console.log({ movies });
   return (
     <div className="container">
       <h1>This is our Front Page</h1>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const { API_URL } = process.env;
+  const res = await fetch(`${API_URL}/movies`);
+  const movies = await res.json();
+
+  return {
+    props: {
+      movies,
+    },
+  };
+};
 
 export default PageIndex;
