be9871fb8d08b3e8180e734950de0cedcd213da4
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-07 18:45:52 +0530

    Navbar header styles with emotion styled

--- src/components/navbar-header.js
+++ src/components/navbar-header.js
@@ -1,5 +1,13 @@
+import styled from "@emotion/styled";
+
 function NavbarHeader() {
-  return <header>Now I am a Real Header!</header>;
+  return <HeaderStyled>Now I am a Real Header!</HeaderStyled>;
 }
+
+const HeaderStyled = styled.header`
+  background: ${({ theme }) => theme.colors.primary};
+  color: white;
+  padding: 1rem;
+`;
 
 export default NavbarHeader;
