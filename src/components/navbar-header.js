87e7e3ac42d1034ee40bf9c71839d1dfb2735b44
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-07 20:21:21 +0530

    Header content & styling including logo

--- src/components/navbar-header.js
+++ src/components/navbar-header.js
@@ -1,13 +1,33 @@
 import styled from "@emotion/styled";
+import { rem } from "polished";
 
 function NavbarHeader() {
-  return <HeaderStyled>Now I am a Real Header!</HeaderStyled>;
+  return (
+    <HeaderStyled>
+      <div className="container">
+        <div className="logo">
+          <img src="/images/logo.svg" alt="Sites logo" />
+          <span className="logo-text">Next Movies</span>
+        </div>
+      </div>
+    </HeaderStyled>
+  );
 }
 
 const HeaderStyled = styled.header`
   background: ${({ theme }) => theme.colors.primary};
-  color: white;
-  padding: 1rem;
+  padding: 20px;
+
+  .logo {
+    display: flex;
+    align-items: center;
+    .logo-text {
+      color: #333333;
+      font-weight: bold;
+      font-size: ${rem(20)};
+      margin-left: 20px;
+    }
+  }
 `;
 
 export default NavbarHeader;
