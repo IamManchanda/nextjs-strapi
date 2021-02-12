651259a161536f63f709362ace03d86948171943
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-12 19:02:32 +0530

    Add default seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,22 @@
-import { ThemeProvider } from "emotion-theming";
 import GlobalStyles from "@/components/global-styles";
 import NavbarHeader from "@/components/navbar-header";
 import theme from "@/theme";
+import { ThemeProvider } from "emotion-theming";
+import { DefaultSeo } from "next-seo";
 import getConfig from "next/config";
+import { Fragment } from "react";
+import SEO from "../../next-seo.config";
 
 function MyApp({ Component, pageProps, navigation }) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <NavbarHeader navigation={navigation} />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Fragment>
+      <DefaultSeo {...SEO} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <NavbarHeader navigation={navigation} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Fragment>
   );
 }
 
