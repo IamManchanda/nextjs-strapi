8f7ac9528c19d3b7a01010090374973d49c36a98
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-13 01:15:24 +0530

    About page custom seo changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,22 @@
+import { NextSeo } from "next-seo";
+import { Fragment } from "react";
+
 function PageAbout() {
-  return "I am an About Page.";
+  const SEO = {
+    title: "About page",
+    description: "Just your normal about page",
+    openGraph: {
+      title: "About page",
+      description: "Just your normal about page",
+    },
+  };
+
+  return (
+    <Fragment>
+      <NextSeo {...SEO} />
+      <h1>I'm an about page</h1>
+    </Fragment>
+  );
 }
 
 export default PageAbout;
