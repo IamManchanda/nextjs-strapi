feadf0a3805922157502e13a7754bcbfa7b3352a
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-13 18:43:41 +0530

    About page with static props

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import { NextSeo } from "next-seo";
 import { Fragment } from "react";
+import { Box } from "reflexbox";
 
-function PageAbout() {
+function PageAbout({ page }) {
   const SEO = {
-    title: "About page",
+    title: page.title,
     description: "Just your normal about page",
     openGraph: {
-      title: "About page",
+      title: page.title,
       description: "Just your normal about page",
     },
   };
@@ -14,9 +15,26 @@ function PageAbout() {
   return (
     <Fragment>
       <NextSeo {...SEO} />
-      <h1>I'm an about page</h1>
+      <Box variant="container">
+        <Box as="h2" my={40}>
+          {page.title}
+        </Box>
+        <div dangerouslySetInnerHTML={{ __html: page.content }} />
+      </Box>
     </Fragment>
   );
 }
+
+export const getStaticProps = async () => {
+  const { NEXT_PUBLIC_API_URL } = process.env;
+  const res = await fetch(`${NEXT_PUBLIC_API_URL}/pages/1`);
+  const page = await res.json();
+
+  return {
+    props: {
+      page,
+    },
+  };
+};
 
 export default PageAbout;
